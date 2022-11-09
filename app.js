4089a6d78ebab76d5ac0f360c91b13dd051d6be0
Artur Castellanos <108274411+arturcastellanos@users.noreply.github.com>
2022-11-09 15:43:32 +0200

    add about and contact pages, make records to be shown on home page

--- app.js
+++ app.js
@@ -5,8 +5,22 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('./public'))
 
+const entries = [{'head': 'Запись 1', 'text': 'djksbgjkagbdgjkls'}, {'head': 'Запись 2', 'text': 'ajsdgj;dsgb'}]
+
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        entries: entries
+    })
 })
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+})
+
+app.get('/')
 
 app.listen(3000);
